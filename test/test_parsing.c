c3f3706928db9669d9120f4565a178dd9807c536
Fin99 <imashkaman@gmail.com>
2019-01-03 19:04:39 +0300

    test: add test for function split

--- test/test_parsing.c
+++ test/test_parsing.c
@@ -7,6 +7,7 @@
 #include "test_parsing.h"
 #include "test.h"
 #include "../parsing-bytecode/file.h"
+#include "../parsing-bytecode/string.h"
 
 char *create_test_file(char *str) {
     char *file_name = "read_file_test";
@@ -27,7 +28,20 @@ test_result_t read_file_test() {
 
     remove(file_name);
 
-    if (strcmp(str_test, str_result) == 0) {
+    if (!strcmp(str_test, str_result)) {
+        return TEST_SUCCESS;
+    } else {
+        return TEST_FAILED;
+    }
+}
+
+test_result_t string_split_test() {
+    char *string = "   Hello my  world    ";
+
+    size_t number_substring;
+    char **split_string = split(string, " ", &number_substring);
+
+    if (!strcmp(split_string[0], "Hello") && !strcmp(split_string[1], "my") && !strcmp(split_string[2], "world")) {
         return TEST_SUCCESS;
     } else {
         return TEST_FAILED;
@@ -37,9 +51,16 @@ test_result_t read_file_test() {
 int main() {
     printf("read_file_test(): \n");
     if (read_file_test() == TEST_SUCCESS) {
-        puts("Test passed successfully");
+        puts("Test passed successfully\n");
     } else {
-        puts("Test failed");
+        puts("Test failed\n");
+    }
+
+    printf("string_split_test(): \n");
+    if (string_split_test() == TEST_SUCCESS) {
+        puts("Test passed successfully\n");
+    } else {
+        puts("Test failed\n");
     }
 }
 
