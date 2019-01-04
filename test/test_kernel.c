a1e768a7f4567b0441b2ea1c88bd9f9fe4db507d
Fin99 <imashkaman@gmail.com>
2019-01-04 22:00:15 +0300

    test(kernel): add class test

--- test/test_kernel.c
+++ test/test_kernel.c
@@ -1,18 +1,15 @@
-//
-// Created by fin on 03.01.19.
-//
-
 #include <stdio.h>
 #include <stdlib.h>
-#include "test_kernel.h"
+#include <string.h>
 #include "test.h"
 #include "../kernel/frame.h"
 #include "../kernel/stack_frame.h"
+#include "../kernel/gc.h"
 
 frame_t *create_test_frame_div_i() {
-    int *args1 = malloc(sizeof(int));
+    long long *args1 = malloc(sizeof(long long));
     args1[0] = 4;
-    int *args2 = malloc(sizeof(int));
+    long long *args2 = malloc(sizeof(long long));
     args2[0] = 15;
 
     instruction_t *instruction1 = constructor_instruction(0, CONST, args1);
@@ -27,6 +24,8 @@ frame_t *create_test_frame_div_i() {
     instructions[3] = instruction4;
 
     frame_t *frame = constructor_frame(0, VOID_RETURN, instructions, 4);
+    frame->name = malloc(sizeof(char) * 5);
+    strcpy(frame->name, "main");
 
     stack_frame_t *stack_frame = get_stack_frame();
     stack_frame->stack_frame[++stack_frame->index_first_element_stack_frame] = frame;
@@ -57,9 +56,9 @@ test_result_t test_kernel_div_i() {
 }
 
 frame_t *create_test_frame_add() {
-    int *args1 = malloc(sizeof(int));
+    long long *args1 = malloc(sizeof(long long));
     args1[0] = 12;
-    int *args2 = malloc(sizeof(int));
+    long long *args2 = malloc(sizeof(long long));
     args2[0] = 4;
 
     instruction_t *instruction1 = constructor_instruction(0, CONST, args1);
@@ -74,6 +73,8 @@ frame_t *create_test_frame_add() {
     instructions[3] = instruction4;
 
     frame_t *frame = constructor_frame(0, VOID_RETURN, instructions, 4);
+    frame->name = malloc(sizeof(char) * 5);
+    strcpy(frame->name, "main");
 
     stack_frame_t *stack_frame = get_stack_frame();
     stack_frame->stack_frame[++stack_frame->index_first_element_stack_frame] = frame;
@@ -103,9 +104,9 @@ test_result_t test_kernel_add() {
 }
 
 frame_t *create_test_frame_mul() {
-    int *args1 = malloc(sizeof(int));
+    long long *args1 = malloc(sizeof(long long));
     args1[0] = 4;
-    int *args2 = malloc(sizeof(int));
+    long long *args2 = malloc(sizeof(long long));
     args2[0] = 5;
 
     instruction_t *instruction1 = constructor_instruction(0, CONST, args1);
@@ -120,6 +121,8 @@ frame_t *create_test_frame_mul() {
     instructions[3] = instruction4;
 
     frame_t *frame = constructor_frame(0, VOID_RETURN, instructions, 4);
+    frame->name = malloc(sizeof(char) * 5);
+    strcpy(frame->name, "main");
 
     stack_frame_t *stack_frame = get_stack_frame();
     stack_frame->stack_frame[++stack_frame->index_first_element_stack_frame] = frame;
@@ -149,9 +152,9 @@ test_result_t test_kernel_mul() {
 }
 
 frame_t *create_test_frame_compare_1() {
-    int *args1 = malloc(sizeof(int));
+    long long *args1 = malloc(sizeof(long long));
     args1[0] = 7;
-    int *args2 = malloc(sizeof(int));
+    long long *args2 = malloc(sizeof(long long));
     args2[0] = 7;
 
     instruction_t *instruction1 = constructor_instruction(0, CONST, args1);
@@ -166,6 +169,8 @@ frame_t *create_test_frame_compare_1() {
     instructions[3] = instruction4;
 
     frame_t *frame = constructor_frame(0, VOID_RETURN, instructions, 4);
+    frame->name = malloc(sizeof(char) * 5);
+    strcpy(frame->name, "main");
 
     stack_frame_t *stack_frame = get_stack_frame();
     stack_frame->stack_frame[++stack_frame->index_first_element_stack_frame] = frame;
@@ -195,9 +200,9 @@ test_result_t test_kernel_compare_1() {
 }
 
 frame_t *create_test_frame_compare_2() {
-    int *args1 = malloc(sizeof(int));
+    long long *args1 = malloc(sizeof(long long));
     args1[0] = 7;
-    int *args2 = malloc(sizeof(int));
+    long long *args2 = malloc(sizeof(long long));
     args2[0] = 14;
 
     instruction_t *instruction1 = constructor_instruction(0, CONST, args1);
@@ -212,6 +217,8 @@ frame_t *create_test_frame_compare_2() {
     instructions[3] = instruction4;
 
     frame_t *frame = constructor_frame(0, VOID_RETURN, instructions, 4);
+    frame->name = malloc(sizeof(char) * 5);
+    strcpy(frame->name, "main");
 
     stack_frame_t *stack_frame = get_stack_frame();
     stack_frame->stack_frame[++stack_frame->index_first_element_stack_frame] = frame;
@@ -241,9 +248,9 @@ test_result_t test_kernel_compare_2() {
 }
 
 frame_t *create_test_frame_invoke() {
-    int *args1 = malloc(sizeof(int));
+    long long *args1 = malloc(sizeof(long long));
     args1[0] = 14;
-    int *args2 = malloc(sizeof(int));
+    long long *args2 = malloc(sizeof(long long));
     args2[0] = 0;
 
     instruction_t *instruction1 = constructor_instruction(0, CONST, args1);
@@ -258,6 +265,8 @@ frame_t *create_test_frame_invoke() {
     instructions[3] = instruction4;
 
     frame_t *frame = constructor_frame(1, VOID_RETURN, instructions, 4);
+    frame->name = malloc(sizeof(char) * 5);
+    strcpy(frame->name, "main");
 
     stack_frame_t *stack_frame = get_stack_frame();
     stack_frame->stack_frame[++stack_frame->index_first_element_stack_frame] = frame;
@@ -267,7 +276,7 @@ frame_t *create_test_frame_invoke() {
 }
 
 frame_t *create_test_frame_invoked() {
-    int *args1 = malloc(sizeof(int));
+    long long *args1 = malloc(sizeof(long long));
     args1[0] = 7;
 
     instruction_t *instruction1 = constructor_instruction(0, CONST, args1);
@@ -278,6 +287,8 @@ frame_t *create_test_frame_invoked() {
     instructions[1] = instruction4;
 
     frame_t *frame = constructor_frame(0, INT_RETURN, instructions, 2);
+    frame->name = malloc(sizeof(char) * 2);
+    strcpy(frame->name, "a");
 
     stack_frame_t *stack_frame = get_stack_frame();
     stack_frame->stack_frame[++stack_frame->index_first_element_stack_frame] = frame;
@@ -310,6 +321,129 @@ test_result_t test_kernel_invoke() {
     1: i_return
      */
 }
+
+frame_t *create_test_frame_class() {
+    long long *index_class_1 = malloc(sizeof(long long));
+    index_class_1[0] = 0;
+
+    long long *index_class_1_local_pool = malloc(sizeof(long long));
+    index_class_1_local_pool[0] = 0;
+    long long *index_class_1_local_pool_1 = malloc(sizeof(long long));
+    index_class_1_local_pool_1[0] = 0;
+    long long *index_class_1_local_pool_2 = malloc(sizeof(long long));
+    index_class_1_local_pool_2[0] = 0;
+
+    long long *number_field_class_1 = malloc(sizeof(long long));
+    number_field_class_1[0] = 0;
+    long long *number_field_class_1_1 = malloc(sizeof(long long));
+    number_field_class_1_1[0] = 0;
+
+
+    long long *index_class_2 = malloc(sizeof(long long));
+    index_class_2[0] = 0;
+
+    long long *index_class_2_local_pool = malloc(sizeof(long long));
+    index_class_2_local_pool[0] = 1;
+    long long *index_class_2_local_pool_1 = malloc(sizeof(long long));
+    index_class_2_local_pool_1[0] = 1;
+    long long *index_class_2_local_pool_2 = malloc(sizeof(long long));
+    index_class_2_local_pool_2[0] = 1;
+
+    long long *number_field_class_2 = malloc(sizeof(long long));
+    number_field_class_1[0] = 0;
+    long long *number_field_class_2_1 = malloc(sizeof(long long));
+    number_field_class_1_1[0] = 0;
+
+    long long *const_value_1 = malloc(sizeof(long long));
+    const_value_1[0] = 9;
+    long long *const_value_2 = malloc(sizeof(long long));
+    const_value_2[0] = 19;
+
+    instruction_t **instructions = malloc(sizeof(instruction_t *) * 16);
+
+    instructions[0] = constructor_instruction(0, NEW, index_class_1);
+    instructions[1] = constructor_instruction(1, STORE, index_class_1_local_pool);
+    instructions[2] = constructor_instruction(2, NEW, index_class_2);
+    instructions[3] = constructor_instruction(3, STORE, index_class_2_local_pool);
+
+    instructions[4] = constructor_instruction(4, CONST, const_value_1);
+    instructions[5] = constructor_instruction(5, LOAD, index_class_1_local_pool_1);
+    instructions[6] = constructor_instruction(6, SET_FIELD, number_field_class_1);
+
+    instructions[7] = constructor_instruction(7, CONST, const_value_2);
+    instructions[8] = constructor_instruction(8, LOAD, index_class_2_local_pool_1);
+    instructions[9] = constructor_instruction(9, SET_FIELD, number_field_class_2);
+
+    instructions[10] = constructor_instruction(10, LOAD, index_class_1_local_pool_2);
+    instructions[11] = constructor_instruction(11, GET_FIELD, number_field_class_1_1);
+
+    instructions[12] = constructor_instruction(12, LOAD, index_class_2_local_pool_2);
+    instructions[13] = constructor_instruction(13, GET_FIELD, number_field_class_2_1);
+
+    instructions[14] = constructor_instruction(14, ADD, NULL);
+    instructions[15] = constructor_instruction(15, RETURN, NULL);
+
+    frame_t *frame = constructor_frame(0, VOID_RETURN, instructions, 16);
+    frame->name = malloc(sizeof(char) * 5);
+    strcpy(frame->name, "main");
+
+    stack_frame_t *stack_frame = get_stack_frame();
+    stack_frame->stack_frame[++stack_frame->index_first_element_stack_frame] = frame;
+    stack_frame->frames[stack_frame->number_frames++] = frame;
+
+    return frame;
+}
+
+void create_test_class() {
+    class_t *class = malloc(sizeof(class_t));
+    class->number_fields = 1;
+    class->index_class = 0;
+    class->name = "Main";
+
+    stack_frame_t *stack_frame = get_stack_frame();
+    stack_frame->classes[stack_frame->number_classes++] = class;
+
+}
+
+test_result_t test_kernel_class() {
+    init_stack_frame();
+    init_gc();
+
+    create_test_class();
+    frame_t *frame = create_test_frame_class();
+    execute_frame(frame);
+
+    if (frame->work_stack[frame->index_first_element_work_stack] == 28) {
+        return TEST_SUCCESS;
+    } else {
+        return TEST_FAILED;
+    }
+    /*
+    main()
+    0: new 0
+    1: store 0
+    2: new 0
+    3: store 1
+
+    4: const 9
+    5: load 0
+    6: set_field 0
+
+    7: const 19
+    8: load 1
+    9: set_field 0
+
+    10: load 0
+    11: get_field 0
+
+    12: load 1
+    13: get_field 0
+
+    12: add
+    13: return
+     */
+}
+
 
 int main() {
     printf("test_kernel_div_i(): \n");
@@ -353,4 +487,13 @@ int main() {
     } else {
         puts("Test failed\n");
     }
+
+    printf("test_kernel_class(): \n");
+    if (test_kernel_class() == TEST_SUCCESS) {
+        puts("Test passed successfully\n");
+    } else {
+        puts("Test failed\n");
+    }
+
+    return 0;
 }
