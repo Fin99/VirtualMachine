26dfe40f43134f061e2d6cbd59c6a6d17fffe9ad
Fin99 <imashkaman@gmail.com>
2019-01-30 00:11:16 +0300

    feat(parser): add test for parser

--- test/test_parsing.c
+++ test/test_parsing.c
@@ -4,44 +4,32 @@
 
 #include <memory.h>
 #include <stdio.h>
-#include "test_parsing.h"
+#include <malloc.h>
+#include <stdbool.h>
 #include "test.h"
-#include "../parsing-bytecode/file.h"
 #include "../parsing-bytecode/string.h"
+#include "../parsing-bytecode/parser.h"
 
-char *create_test_file(char *str) {
-    char *file_name = "read_file_test";
-
-    FILE *file = fopen(file_name, "w+b");
-
-    fwrite(str, strlen(str), 1, file);
-
-    fclose(file);
-    return file_name;
-}
-
-test_result_t read_file_test() {
-    char *str_test = "test\nme";
-    char *file_name = create_test_file(str_test);
-
-    const char *str_result = read_file(file_name);
-
-    remove(file_name);
-
-    if (!strcmp(str_test, str_result)) {
-        return TEST_SUCCESS;
-    } else {
-        return TEST_FAILED;
-    }
-}
-
-test_result_t string_split_test() {
+test_result_t test_string_split() {
     char *string = "   Hello my  world    ";
 
     size_t number_substring;
     char **split_string = split(string, " ", &number_substring);
 
     if (!strcmp(split_string[0], "Hello") && !strcmp(split_string[1], "my") && !strcmp(split_string[2], "world")) {
+        for (int i = 0; i < number_substring; ++i) {
+            free(split_string[i]);
+        }
+        free(split_string);
+        return TEST_SUCCESS;
+    } else {
+        return TEST_FAILED;
+    }
+}
+
+test_result_t test_parser() {
+    load_class_and_frame("SumTest.fn");
+    if (true) {
         return TEST_SUCCESS;
     } else {
         return TEST_FAILED;
@@ -49,18 +37,26 @@ test_result_t string_split_test() {
 }
 
 int main() {
-    printf("read_file_test(): \n");
-    if (read_file_test() == TEST_SUCCESS) {
-        puts("Test passed successfully\n");
-    } else {
-        puts("Test failed\n");
+    int number_test_functions = 2;
+    test_result_t(*test_functions[number_test_functions])();
+    test_functions[0] = test_string_split;
+    test_functions[1] = test_parser;
+
+    char *name_test_functions[number_test_functions];
+    name_test_functions[0] = "test_string_split():";
+    name_test_functions[1] = "test_parser():";
+
+    for (int i = 0; i < number_test_functions; ++i) {
+        puts(name_test_functions[i]);
+        if (test_functions[i]() == TEST_SUCCESS) {
+            puts("Test passed successfully\n");
+            fflush(stdout);
+        } else {
+            fputs("Test failed\n", stderr);
+            fflush(stderr);
+        }
+        puts("-----------------------------------");
     }
 
-    printf("string_split_test(): \n");
-    if (string_split_test() == TEST_SUCCESS) {
-        puts("Test passed successfully\n");
-    } else {
-        puts("Test failed\n");
-    }
+    return 0;
 }
-
